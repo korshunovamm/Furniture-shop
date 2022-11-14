122cb1d4e83784625d90fbd76dbb30c4a80f850e
korshunovamm <korshunova.mm@phystech.edu>
2022-11-14 10:32:15 +0300

    change position, make more flexible for scaling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Main} from './Components/Main';
+
+
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Main />
+        {/* <Routes>
+          <Route path='' element={<Main />}></Route>
+          <Route path='/catalog' element={<Catalog />}></Route>
+          <Route path='/service' element={<Service />}></Route>
+        </Routes> */}
+      </BrowserRouter>
+    </>
   );
 }
 
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,7 +7,6 @@ import FrontImg from '../UI/Images/MainPage/front_img.png';
 import BackImg from '../UI/Images/MainPage/back_img.png';
 import GoToCatalog from '../UI/Buttons/MainPage/go_to_catalog.png';
 
-
 export function Main() {
   const location = useLocation();
   const mainHeaderNavIcons = {
@@ -18,75 +17,77 @@ export function Main() {
 
   return (
     <>
-      <aside className='canvas'></aside>
-      <h id='sweet-home'> SweetHome </h>
       <aside className='main'>
+        <header className='header'>
+          <div id='sweet-home'> SweetHome</div>
+          <nav className='main-nav-links'>
+            {['main', 'catalog', 'service'].map((mainHeaderNavLink) => (
+              <div
+                id={`main-nav-${mainHeaderNavLink}`}
+                className={
+                  location.pathname === `/${mainHeaderNavLink}`
+                    ? 'main-nav-link-active'
+                    : 'main-nav-link'
+                }
+              >
+                <Link to={`/${mainHeaderNavLink}`} style={{ color: '#EBE9E9' }}>
+                  {mainHeaderNavLink.charAt(0).toUpperCase() +
+                    mainHeaderNavLink.slice(1)}
+                </Link>
+              </div>
+            ))}
+          </nav>
 
-        <nav className='main-nav-links'>
-          {['main', 'catalog', 'service'].map((mainHeaderNavLink) => (
-            <div
-              id={`main-nav-${mainHeaderNavLink}`}
-              className={
-                location.pathname === `/${mainHeaderNavLink}`
-                  ? 'main-nav-link-active'
-                  : 'main-nav-link'
-              }
-            >
-              <Link to={`/${mainHeaderNavLink}`} style={{ color: '#EBE9E9' }}>
-                {mainHeaderNavLink.charAt(0).toUpperCase() +
-                  mainHeaderNavLink.slice(1)}
-              </Link>
-            </div>
-          ))}
-        </nav>
+          <nav className='main-nav-icons'>
+            {['search', 'cart', 'profile'].map((mainHeaderNavIcon) => (
+              <div
+                id={`main-header-nav-${mainHeaderNavIcon}`}
+                className={
+                  location.pathname === `/${mainHeaderNavIcon}`
+                    ? 'main-nav-icon-active'
+                    : 'main-nav-icon'
+                }
+              >
+                <Link to={mainHeaderNavIcon}>
+                  <img
+                    src={mainHeaderNavIcons[mainHeaderNavIcon]}
+                    alt={mainHeaderNavIcon}
+                  />
+                </Link>
+              </div>
+            ))}
+          </nav>
+        </header>
 
-        <nav className='main-nav-icons'>
-          {['search', 'cart', 'profile'].map((mainHeaderNavIcon) => (
-            <div
-              id={`main-header-nav-${mainHeaderNavIcon}`}
-              className={
-                location.pathname === `/${mainHeaderNavIcon}`
-                  ? 'main-nav-icon-active'
-                  : 'main-nav-icon'
-              }
-            >
-              <Link to={mainHeaderNavIcon}>
-                <img
-                  src={mainHeaderNavIcons[mainHeaderNavIcon]}
-                  alt={mainHeaderNavIcon}
-                />
-              </Link>
-            </div>
-          ))}
-        </nav>
+        <aside className='content'>
+          <section className='photos'>
+            <picture className='rectangle'>
+              <img id='back_img' src={BackImg} alt='img' />
+            </picture>
+            <picture className='circle'>
+              <img id='front_img' src={FrontImg} alt='img' />
+            </picture>
 
+          </section>
 
-        <section className='main-content'>
-          <div className='main-text'>
-            <header id='main-content-header'>
+          <section className='main-content'>
+            <div className='main-content-text'>
+            <header id='main-content-text-header'>
               Everything that surround us makes us stronger
             </header>
-            <p id='main-content-text'>
+            <p id='main-content-text-text'>
               Exclusive designer furniture and fittings. Natural materials and
               individual approach when created, they will give an incomparably
               high level of quality and comfort
             </p>
           </div>
 
-          <button id='main-content-button'>
+            <button id='main-content-button'>
             <Link to='/catalog'>
               <img src={GoToCatalog} alt='Go to Catalog'></img>
             </Link>
           </button>
-        </section>
-
-        <aside className='photos'>
-          <picture className='rectangle'>
-            <img id='back_img' src={BackImg} alt='img' />
-          </picture>
-          <picture className='elipse'>
-            <img id='front_img' src={FrontImg} alt='img' />
-          </picture>
+          </section>
         </aside>
       </aside>
     </>
